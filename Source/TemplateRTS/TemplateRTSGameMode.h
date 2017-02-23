ab1a08224398a44b82ca70f4f914b3e9b5cb46a2
ETU\a.gargasson <a.gargasson@rubika-edu.com>
2017-02-23 15:06:59 +0100

    first commit

---
+++ Source/TemplateRTS/TemplateRTSGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TemplateRTSGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEMPLATERTS_API ATemplateRTSGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
